359fd2366090b31b68511fa8316bc30997c0bcf7
Phan Thi Linh Tra <phanthilinhtra25@gmail.com>
2025-11-30 19:33:33 +0100

    save

--- JaratKezeloProject/Tests/TestJaratKezeloProject.cs
+++ JaratKezeloProject/Tests/TestJaratKezeloProject.cs
@@ -15,7 +15,17 @@ namespace JaratKezeloProject.Tests
 		public void SetUp()
 		{
 			jaratKezelo = new JaratKezelo();
-			jaratKezelo.UjJarat();
 		}
-	}
+		[Test]
+        public void UjJarat()
+        {
+            JaratKezelo jk = new JaratKezelo();
+            jk.UjJarat("XY123", "BUD", "NYC", new DateTime(2025, 1, 1, 12, 00, 00));
+
+            var indul = jk.MikorIndul("XY123");
+
+            Assert.That(indul, Is.EqualTo(new DateTime(2025, 1, 1, 12, 00, 00)));
+        }
+
+    }
 }
