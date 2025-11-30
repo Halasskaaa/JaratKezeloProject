a79f12abb44691a3e18ed9c208a01c7d766e5e16
Phan Thi Linh Tra <phanthilinhtra25@gmail.com>
2025-11-30 19:13:54 +0100

    update

---
+++ JaratKezeloProject/Jarat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaratKezeloProject
+{
+    internal class Jarat
+    {
+        public string JaratSzam { get; set; }
+        public string RepterHonnan { get; set; }
+        public string RepterHova { get; set; }
+        public DateTime Indulas { get; set; }
+        public int Keses { get; set; } = 0;
+    }
+}
