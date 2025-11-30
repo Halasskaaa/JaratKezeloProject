393962689e54986926ddb8a21ecd8c847219fc39
Phan Thi Linh Tra <phanthilinhtra25@gmail.com>
2025-11-30 19:38:35 +0100

    save

--- JaratKezeloProject/Tests/TestJaratKezeloProject.cs
+++ JaratKezeloProject/Tests/TestJaratKezeloProject.cs
@@ -7,25 +7,68 @@ using System.Threading.Tasks;
 
 namespace JaratKezeloProject.Tests
 {
-	internal class TestJaratKezeloProject
-	{
-		JaratKezelo jaratKezelo;
-
-		[SetUp]
-		public void SetUp()
-		{
-			jaratKezelo = new JaratKezelo();
-		}
-		[Test]
-        public void UjJarat()
+    [TestFixture]
+    internal class JaratKezeloTest
+    {
+        [Test]
+        public void UjJarat_HozzaadodikASikeresenLetrehozottJarat()
         {
             JaratKezelo jk = new JaratKezelo();
-            jk.UjJarat("XY123", "BUD", "NYC", new DateTime(2025, 1, 1, 12, 00, 00));
+            jk.UjJarat("ABC123", "BUD", "LAX", new DateTime(2025, 1, 1, 8, 0, 0));
 
-            var indul = jk.MikorIndul("XY123");
+            var jaratok = jk.Jaratok();
 
-            Assert.That(indul, Is.EqualTo(new DateTime(2025, 1, 1, 12, 00, 00)));
+            Assert.That(jaratok.Count, Is.EqualTo(1));
         }
 
+        [Test]
+        public void Keses_NoveliAzIndulasiIdot()
+        {
+            JaratKezelo jk = new JaratKezelo();
+            jk.UjJarat("ABC123", "BUD", "LAX", new DateTime(2025, 1, 1, 8, 0, 0));
+
+            jk.Keses("ABC123", 30);
+
+            var indulasiIdo = jk.IndulasiIdo("ABC123");
+
+            Assert.That(indulasiIdo, Is.EqualTo(new DateTime(2025, 1, 1, 8, 30, 0)));
+        }
+
+        [Test]
+        public void Keses_LevonasEsebenNemCsokkenhetNullaAlatt()
+        {
+            JaratKezelo jk = new JaratKezelo();
+            jk.UjJarat("ABC123", "BUD", "LAX", new DateTime(2025, 1, 1, 8, 0, 0));
+
+            jk.Keses("ABC123", 20);
+            jk.Keses("ABC123", -50);
+
+            var indulasiIdo = jk.IndulasiIdo("ABC123");
+
+            Assert.That(indulasiIdo, Is.EqualTo(new DateTime(2025, 1, 1, 8, 0, 0)));
+        }
+
+        [Test]
+        public void UjJarat_HibasHaMarLetezikIlyenJarat()
+        {
+            JaratKezelo jk = new JaratKezelo();
+            jk.UjJarat("ABC123", "BUD", "LAX", DateTime.Now);
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                jk.UjJarat("ABC123", "BUD", "LAX", DateTime.Now);
+            });
+        }
+
+        [Test]
+        public void IndulasiIdo_HelyesIdotAdVissza()
+        {
+            JaratKezelo jk = new JaratKezelo();
+            var idopont = new DateTime(2025, 1, 1, 8, 0, 0);
+
+            jk.UjJarat("DEF456", "BUD", "NYC", idopont);
+
+            Assert.That(jk.IndulasiIdo("DEF456"), Is.EqualTo(idopont));
+        }
     }
 }
